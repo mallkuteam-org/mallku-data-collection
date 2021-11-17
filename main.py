a4551202d74b7ae1751be7edbf6b8881ce0d1a2a
Facundo Esquivel Fagiani <facufref@gmail.com>
2021-11-17 17:22:59 -0300

    Api Requests Example

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# import numpy as np
+import api_requests as req
 
 
 def main():
-    # print(np.mean([5, 3, 8]))
-    print("Hello Mallku Team")
+    # json_result = req.request_breeds_by_name("pers", True)
+    req.post_my_vote(image_id="8rm", sub_id="api-facu", value=3)
+    json_votes = req.request_my_votes(sub_id="api-facu")
+    print(json_votes)
 
 
 if __name__ == '__main__':
     main()
+

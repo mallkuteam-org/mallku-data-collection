67dabc1778aca831a30785e14eb031bb0cb54f18
Damian <damiiann95@gmail.com>
2021-12-06 19:36:26 -0300

    read and show elements from XMl in console

--- xml_processing.py
+++ xml_processing.py
@@ -4,10 +4,22 @@ import xml.etree.ElementTree as ET
 def process_xml():
     tree = ET.parse('utils/openSearchExample.xml')
     root = tree.getroot()
-    for child in root:
-        print(f"tag: {child.tag} attrib: {child.text}")
-        # child.attrib['href'] funciona para obtener el link solo si el tag tiene el atributo href
 
+    for node in root.iter('{http://www.w3.org/2005/Atom}feed'):
+        for element in node.iter():
+           if element.tag=="{http://www.w3.org/2005/Atom}title":
+                print('-----------------------------')
+                print('title: ', element.text)
+           if element.tag == "{http://www.w3.org/2005/Atom}link":
+                print('link: ', element.attrib['href'])
+           if element.attrib=={'name':'ingestiondate'}:
+                print('Ingestion Date: ', element.text)
+           if element.attrib=={'name':'size'}:
+                print('Size: ',element.text)
+           if element.attrib=={'name':'processinglevel'}:
+                print('Processing Level: ', element.text)
+           if element.attrib=={'name':'uuid'}:
+                print('uuid: ', element.text)
 
 if __name__ == '__main__':
     process_xml()

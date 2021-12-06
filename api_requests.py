1c260431b063bd86af4dd41d1d152b152b202aab
martin nievas <martin.nievas.ar@gmail.com>
2021-12-06 17:17:38 -0300

    Add coords as a parameters in query

--- api_requests.py
+++ api_requests.py
@@ -7,7 +7,10 @@ def make_query(username, password):
         "password": password
     }
 
+    coordinates = [-31.4166, -64.1833]
+    coords = 'footprint:"Intersects(' + str(coordinates[0]) + ',' + str(coordinates[1]) + ')"'
+
     response = requests.get(
-        "https://scihub.copernicus.eu/dhus/search?q=footprint:%22Intersects(-31.4166,%20-64.1833)%22",
+        'https://scihub.copernicus.eu/dhus/search?q=' + coords,
         auth=requests.auth.HTTPBasicAuth(auth['user'], auth['password']))
     return response

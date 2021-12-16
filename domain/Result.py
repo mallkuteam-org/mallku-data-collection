53184cbf3cde8648173211d16818225b4a4e9c7d
Facundo Esquivel Fagiani <facufref@gmail.com>
2021-12-16 18:54:32 -0300

    Integrate link generation and implement csv files

---
+++ domain/Result.py
@@ -0,0 +1,9 @@
+class Result:
+    def __init__(self):
+        self.link = None
+        self.title = None
+        self.ingestion_date = None
+        self.processing_level = None
+        self.size = None
+        self.uuid = None
+        self.is_downloaded = False

12a67b93979d55c87d0152b54c94249475bb1261
martin nievas <martin.nievas.ar@gmail.com>
2021-12-17 14:39:11 -0300

    Remove is_downloaded assign

--- copernicus_requests.py
+++ copernicus_requests.py
@@ -88,7 +88,6 @@ def get_processed_xml(xml_string):
             if element.attrib == {'name': 'uuid'} and result.uuid is None:
                 print('uuid: ', element.text)
                 result.uuid = element.text
-            result.is_downloaded = False
         results.append(result)
     return results
 

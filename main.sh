571e6d60a0ee01a70896c98e90c3cf6d90129301
martin nievas <martin.nievas.ar@gmail.com>
2021-12-20 14:58:15 -0300

    Rename is_downloaded column and specify python version

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python main.py
+python3 main.py
 
 FILENAME="utils/links.csv"
 
-while IFS="," read -r link title ingestion processing_level size uuid downloaded username password
+while IFS="," read -r link title ingestion processing_level size uuid is_downloaded username password
 do
   echo "link: $link "
   echo "title: $title"
@@ -12,7 +12,7 @@ do
   echo "processing level: $processing_level"
   echo "size: $size"
   echo "uuid: $uuid"
-  echo "downloaded: $downloaded"
+  echo "is_downloaded: $is_downloaded"
   echo "username: $username"
   echo "password: $password"
   echo ""
